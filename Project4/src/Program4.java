1f4d9652bbf5b27948f890386fd44a7d3f98008e
Oblivion1221 <oblivion1221@hotmail.com>
2017-10-21 12:37:33 -0400

    hw completed

--- Project4/src/Program4.java
+++ Project4/src/Program4.java
@@ -1,40 +1,50 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program4 {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        keyboard.useDelimiter(":");
 
         boolean again = true;
 
         while (again) {
             System.out.println("Enter time in 24-hour notation:");
+
             try {
-                while (keyboard.hasNext()) {
-                    int hr = Integer.parseInt(keyboard.next());
-                    int min = Integer.parseInt(keyboard.next());
-                }
+                String input = keyboard.next();
+
+                int hr = Integer.parseInt(input.split(":")[0]);
+                int min = Integer.parseInt(input.split(":")[1]);
+
                 if (min >= 60 || hr >= 24) {
                     throw new TimeFormatException("That is not a valid time\n\nTry again:");
                 }
+
                 System.out.println("That is the same as");
-                if (hr >= 12)
-                    System.out.println((hr - 12) + ":" + min + "PM");
+
+                if (hr > 12)
+                    System.out.println((hr - 12) + ":" + min + " PM");
+                else if (hr == 12)
+                    System.out.println(hr + ":" + min + " PM");
+                else if (hr == 0)
+                    System.out.println(12 + ":" + min + " AM");
                 else
-                    System.out.println(hr + ":" + min + "AM");
+                    System.out.println(hr + ":" + min + " AM");
+
                 System.out.println("\nAgain? (y/n)");
                 String op = keyboard.next();
-                if (!op.equals("Y") || !op.equals("y")) {
+
+                if (!op.toLowerCase().equals("y")) {
                     again = false;
                     System.out.println("End of Program");
+                } else {
+                    System.out.println();
                 }
-            } catch (TimeFormatException e) {
+            } catch (TimeFormatException e1) {
                 keyboard.nextLine();
-                System.out.println(e.getMessage());
-            } catch (InputMismatchException m) {
+                System.out.println(e1.getMessage());
+            } catch (Exception e3) {
                 keyboard.nextLine();
-                System.out.println("That is not a valid time\n\nTry again:");
+                System.out.println("Please enter the time in the format of (int)hr:(int)min\n\nTry again:");
             }
         }
     }
