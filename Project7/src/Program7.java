e3c7017b1432367d93d9014cfdeb1e7f8af30cbb
0b1ivioN <Oblivion1221@hotmail.com>
2017-11-14 12:28:18 -0500

    Update Program7.java

--- Project7/src/Program7.java
+++ Project7/src/Program7.java
@@ -1,17 +1,24 @@
 // Yaoxian Yang
-// COSC 211 Programming Assignment #7
+// COSC 211 Programming Assignment 
+// Lab #7
 // Fall 2017: 12:00 ~ 13:50 Tue, Thu
 // A method that counts the frequency of
 // occurrence of a digit in an int number
 
+import java.util.Scanner;
+
 public class Program7 {
     public static void main(String[] args) {
-        System.out.println(frequencyCount(1342457, 4));
-        System.out.println(frequencyCount(1342457, 9));
+    		Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = keyboard.nextInt();
+        System.out.println("Enter the digit you want to count:");
+        int d = keyboard.nextInt();
+        System.out.println(d + " occurs for " + frequencyCount(n, d) + " times.");
     }
 
     public static int frequencyCount(int n, int d) {
-        if (n == 0 && n != d) {
+        if (n == 0) {
             return 0;
         } else if (n % 10 == d) {
             return 1 + frequencyCount(n / 10, d);
@@ -21,7 +28,17 @@ public class Program7 {
     }
 }
 
-// Output:
-// 2
-// 0
-//
+/* 
+ * Enter a number:
+ * 62656
+ * Enter the digit you want to count:
+ * 3
+ * 3 occurs for 0 times.
+ * 
+ * Enter a number:
+ * 98888888
+ * Enter the digit you want to count:
+ * 8
+ * 8 occurs for 7 times.
+ * 
+ */
