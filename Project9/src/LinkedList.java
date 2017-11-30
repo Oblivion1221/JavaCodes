e10f0fda6b68cab0f52fcb241de19a603a802ed2
Oblivion1221 <oblivion1221@hotmail.com>
2017-11-30 14:55:21 -0500

    1.0

--- Project9/src/LinkedList.java
+++ Project9/src/LinkedList.java
@@ -1,5 +1,83 @@
-public class LinkedList {
-    public class Node {
+class LinkedList {
+    private class Node {
+        int item;
+        Node link;
 
+        Node(int item, Node link) {
+            this.item = item;
+            this.link = link;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    void addToStart(int item) {
+        if (head == null) {
+            head = new Node(item, null);
+            tail = head;
+        } else {
+            head = new Node(item, head);
+        }
+    }
+
+    void addToEnd(int item) {
+        Node tmp = new Node(item, null);
+        if (tail == null) {
+            tail = tmp;
+            head = tail;
+        } else {
+            tail.link = tmp;
+            tail = tail.link;
+        }
+    }
+
+    void printLength() {
+        int cnt = 0;
+        Node pos = head;
+
+        while (pos != null) {
+            cnt++;
+            pos = pos.link;
+        }
+
+        System.out.println("The length of the linked list is " + cnt);
+    }
+
+    boolean contains(int n) {
+        Node pos = head;
+        while (pos != null) {
+            if (pos.item == n)
+                return true;
+            pos = pos.link;
+        }
+        return false;
+    }
+
+    void delete(int n) {
+        Node pos = head;
+        while (this.contains(n) && pos != null) {
+            if (pos.link == null)
+                pos = null;
+            else if (pos.link.item == n)
+                pos.link = pos.link.link;
+            else
+                pos = pos.link;
+        }
+    }
+
+    void printList() {
+        Node pos = head;
+        if (pos == null) System.out.println("This is an empty list.");
+        while (pos != null) {
+            System.out.print(pos.item + " ");
+            pos = pos.link;
+        }
+        System.out.println();
     }
 }
--- Project9/src/Program9.java
+++ Project9/src/Program9.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class Program9 {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        LinkedList L = new LinkedList();
 
+        int op = -1;
+        while (op != 0) {
+            System.out.println("Available Operations: ");
+            System.out.println("0.  Quit the program");
+            System.out.println("1.  Insert a number at the beginning of a list");
+            System.out.println("2.  Insert a number at the end of a list");
+            System.out.println("3.  Display the length of the list");
+            System.out.println("4.  Search the list for a specific number(for membership)");
+            System.out.println("5.  Delete a node containing a specific number from the list");
+            System.out.println("6.  Print the list");
+
+            op = keyboard.nextInt();
+
+            switch (op) {
+                case 1:
+                    System.out.println("Enter the number to be added to the beginning:");
+                    int tmp1 = keyboard.nextInt();
+                    L.addToStart(tmp1);
+                    break;
+                case 2:
+                    System.out.println("Enter the number to be added to the end:");
+                    int tmp2 = keyboard.nextInt();
+                    L.addToEnd(tmp2);
+                    break;
+                case 3:
+                    L.printLength();
+                    break;
+                case 4:
+                    System.out.println("Enter the number to be searched:");
+                    int tmp3 = keyboard.nextInt();
+                    if (L.contains(tmp3))
+                        System.out.println(tmp3 + " exists in the linked list");
+                    else
+                        System.out.println(tmp3 + " does not exist in the linked list");
+                    break;
+                case 5:
+                    System.out.println("Enter the number to be deleted:");
+                    int tmp4 = keyboard.nextInt();
+                    if (!L.contains(tmp4)) {
+                        System.out.println(tmp4 + " does not exist in the linked list");
+                        break;
+                    }
+                    L.delete(tmp4);
+                    break;
+                case 6:
+                    L.printList();
+                    break;
+            }
+        }
     }
 }
